969b503623bd58bc3222472c11d0ba62e3236ff5
AlilyRei <alymariecabahug@gmail.com>
2023-11-05 21:35:28 +0800

    added new page

--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,110 @@
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+/*import 'package:tflite_flutter/tflite_flutter.dart';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+import 'package:image/image.dart' as img;
+*/
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final cameras = await availableCameras();
+  runApp(MyApp(cameras: cameras));
 }
+
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  @override
+  final List<CameraDescription> cameras;
+  const MyApp({super.key, required this.cameras});
+
+ @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-       
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return MaterialApp( 
+      home: InitPage(cameras: cameras),
     );
   }
+
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-   
-      _counter++;
-    });
-  }
+class InitPage extends StatelessWidget {
+  final List<CameraDescription> cameras;
+  
+  const InitPage({Key? key, required this.cameras}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-  
+    double scrWidth = MediaQuery.of(context).size.width;
+    double scrHeight = MediaQuery.of(context).size.height;
+
+
     return Scaffold(
-      appBar: AppBar(
-       
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-       
-        title: Text(widget.title),
-      ),
-      body: Center(
-        
-        child: Column(
-        
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+
+
+      body: Container( 
+      color: Color.fromARGB(255, 230, 195, 240),
+      child: Column(
+        children: [ 
+          Center(
+            child: Container( 
+              width: scrWidth,
+              margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
+              padding: const EdgeInsets.all(10),
+              //margin: EdgeInsets.all(15),
+              height: scrHeight * 0.50,
+              foregroundDecoration: const BoxDecoration(
+                image: DecorationImage(image: AssetImage("assets/images/splash_screen.png"), fit: BoxFit.fill),
+                
+                borderRadius: BorderRadius.all(Radius.circular(45.5))
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          
             ),
-          ],
-        ),
+          ),
+
+         
+
+          Text("About Us: ", 
+          style: TextStyle(
+              fontSize: 24,
+              fontFamily: "Arial",
+              fontWeight: FontWeight.bold
+            ),
+            ),
+          Text("We redefine fish freshness using AI and image recognition tech.\nEmpowering consumers to choose the freshest fish.\nHow It Works: AI analyzes fish eyes for a precise freshness score.",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 14,
+              fontFamily: "Arial",
+              fontWeight: FontWeight.bold
+            ),
+          ),
+
+          ElevatedButton.icon(
+          style: ButtonStyle(
+          iconColor: MaterialStatePropertyAll<Color>(Color.fromARGB(255, 230, 195, 240)),
+          backgroundColor: MaterialStatePropertyAll<Color>(Color.fromARGB(255, 88, 32, 100)),
+          ),
+          onPressed: () {
+            
+          },
+          icon: const Icon(Icons.arrow_circle_right_rounded),
+          label: Container(
+              padding: const EdgeInsets.all(4),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  border: Border.all(),
+                  color: const Color.fromARGB(255, 230, 195, 240)),
+              child: const Text(
+                'Proceed',
+                style: TextStyle(
+                    fontFamily: 'Helvetica',
+                    color: Color.fromARGB(255, 27, 25, 26))),
+              )),
+          
+        
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
+      )
     );
   }
 }
